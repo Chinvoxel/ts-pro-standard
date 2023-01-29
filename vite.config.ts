ae4b779f23814e9152b83720d430b00ab22d399d
Chinvoxel <1350106846@qq.com>
2023-01-29 15:53:20 +0800

    perf(全局): 优化打包性能、配置排除打包第三方依赖,减小打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import viteCompression from 'vite-plugin-compression' // 压缩插件
 import visualizer from 'rollup-plugin-visualizer' // 打包体积分析插件
+import externalGlobals from 'rollup-plugin-external-globals' // 排除第三方依赖打包
 
 // https://vitejs.dev/config/
-export default ({ mode }) =>
-  defineConfig({
-    base: './', // 开发或生产环境服务的公共基础路径
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    base: env.VITE_APP_BASE_API, // 开发或生产环境服务的公共基础路径
     // 插件
     plugins: [
       vue(),
@@ -31,7 +33,7 @@ export default ({ mode }) =>
       // 别名
       alias: {
         '@': path.resolve(__dirname, 'src'),
-        '/img': './src/assets/images/'
+        '/img': path.resolve(__dirname, 'src/assets/images/')
       },
       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'] // 文件后缀拓展
     },
@@ -48,6 +50,19 @@ export default ({ mode }) =>
     build: {
       chunkSizeWarningLimit: 1600, // 限制分包大小
       rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: ['vue', 'vue-router', 'element-plus', 'axios', 'vue-demi'],
+        plugins: [
+          // 不打包依赖映射的对象
+          externalGlobals({
+            vue: 'Vue',
+            'vue-router': 'VueRouter',
+            'element-plus': 'ElementPlus',
+            axios: 'axios',
+            '@element-plus/icons-vue': 'ElementPlusIconsVue',
+            'vue-demi': 'VueDemi'
+          })
+        ],
         output: {
           chunkFileNames: 'static/js1/[name]-[hash].js',
           entryFileNames: 'static/js2/[name]-[hash].js',
@@ -75,10 +90,11 @@ export default ({ mode }) =>
       // 代理
       proxy: {
         '/api': {
-          target: loadEnv(mode, process.cwd()).VITE_APP_BASE_API, // 使用环境变量
+          target: env.VITE_APP_SERVER_API, // 使用环境变量
           changeOrigin: true,
           rewrite: paths => paths.replace(/^\/api/, '')
         }
       }
     }
   })
+}
