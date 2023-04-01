fae93c67a05b46366862781f702f81dd1406aabf
Chinvoxel <1350106846@qq.com>
2023-04-01 23:12:59 +0800

    chore(custom): 优化eslint配置
    优化eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     node: true,
     'vue/setup-compiler-macros': true // 解决defineProps、defineEmits 报错未定义的问题
   },
+  // 配置js全局变量，
+  globals: {},
   extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
   parser: 'vue-eslint-parser',
   parserOptions: {
@@ -14,13 +16,27 @@ module.exports = {
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'vue/no-multiple-template-root': 'off', // 关闭多根节点检测
-    'no-console': 'off', // 关闭禁止存在打印
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }], // 关闭错误提示开发依赖需安装为生产依赖
     'import/prefer-default-export': 'off', // 关闭推荐export default
+    // 解决无法识别vite别名导致的错误
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['^@']
+      }
+    ],
+
+    /* VUE */
+    'vue/no-multiple-template-root': 'off', // 关闭多根节点检测
     'vue/comment-directive': 'off',
     'vue/multi-word-component-names': 'off', // 关闭单文件组件名必须多个单词的限制
-    // 关闭错误提示开发依赖需安装为生产依赖
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+
+    /* JS */
+    'no-console': 'off', // 关闭禁止存在打印
+    'no-use-before-define': ['error', { variables: false, functions: false }], // 关闭变量、函数定义前使用报错提示
+    'no-restricted-syntax': 'off', // 关闭禁用for-in for-of的规则
+    'no-param-reassign': ['error', { props: false }], // 关闭禁止直接修改函数的入参
+
     // 解决后缀忽略不识别问题
     'import/extensions': [
       'error',
@@ -30,14 +46,9 @@ module.exports = {
         ts: 'never',
         tsx: 'never'
       }
-    ],
-    // 关闭变量、函数定义前使用报错提示
-    'no-use-before-define': ['error', { variables: false, functions: false }],
-    // 关闭禁用for-in for-of的规则
-    'no-restricted-syntax': 0,
-    // 关闭禁止直接修改函数的入参
-    'no-param-reassign': ['error', { props: false }]
+    ]
   },
+
   settings: {
     'import/core-modules': ['virtual:windi.css'], // 解决main引入windi报错的问题
     // 解决后缀忽略不识别问题
